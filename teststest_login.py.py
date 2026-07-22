3f8e017a646c123be5dd1265bf404947995bbe61
Nandhanasr-18 <nandhuchandran2006@gmail.com>
2026-07-22 03:28:03 -0700

    Delete teststest_login.py.py

--- teststest_login.py.py
+++
@@ -1,5 +0,0 @@
-def test_dummy_authentication():
-    """Test sample login validation logic."""
-    username = "admin"
-    password = "admin123"
-    assert username == "admin" and password == "admin123"
