b7898bf484a16b3e4baf455bf853350ee28e94d0
Nandhanasr-18 <nandhuchandran2006@gmail.com>
2026-07-22 02:35:38 -0700

    Delete componentsfilters.py.py

--- componentsfilters.py.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def render_facility_filter(building_names: list):
-    """Dropdown filter component for facility targets."""
-    return st.selectbox("Select Target Facility", ["All Facilities"] + building_names)
