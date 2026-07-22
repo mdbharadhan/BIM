8bb7939867aa4de42a67e04806034f3325ee1894
Deepi0818 <deepikalp8@gmail.com>
2026-07-22 07:44:14 +0100

    Delete teststest_reports.py.py

--- teststest_reports.py.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def test_csv_export_generation():
-    """Test Pandas CSV formatting."""
-    df = pd.DataFrame([{"code": "BLD-01", "name": "AeroTech HQ"}])
-    csv_bytes = df.to_csv(index=False).encode('utf-8')
-    assert len(csv_bytes) > 0
