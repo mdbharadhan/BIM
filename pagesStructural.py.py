95423afc6cc1adde8904077b720f02e005737cd3
Nandhanasr-18 <nandhuchandran2006@gmail.com>
2026-07-22 03:26:01 -0700

    Delete pagesStructural.py.py

--- pagesStructural.py.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-from components.structural import render_structural
-
-st.set_page_config(page_title="Structural Integrity", page_icon="🦴", layout="wide")
-render_structural()
