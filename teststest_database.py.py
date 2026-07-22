527e169d9c573f52de53704079a49acc1352c99a
Nandhanasr-18 <nandhuchandran2006@gmail.com>
2026-07-22 03:27:55 -0700

    Delete teststest_database.py.py

--- teststest_database.py.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-from config import DB_PATH
-
-def test_database_connection():
-    """Verify local database file connectivity."""
-    conn = sqlite3.connect(DB_PATH)
-    assert conn is not None
-    conn.close()
